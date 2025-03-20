1f1906eaa573cca92a5b8623266db47be9f53bc5
had-nu <gustavo.leaomelo@gmail.com>
2025-03-20 15:38:07 +0000

    logo changed

--- internal/ui/banner.go
+++ internal/ui/banner.go
@@ -1,24 +1,23 @@
-package uint
+package ui
 
 import "fmt"
 
 func PrintBanner() {
 	banner := `
-	 nNNNn  nNNNn       sSSSs
-     N//// ///// S/////
-     N////n/////nS/////
-     N////////////S////
-     N////////////S///s
-     N////////////S//Ss
-     N///nNNNn///S/Ss     | Welcome to NameSniper
-     N/// NNN ///S/Ss     | Version 1.0
-     N/// NNN ///S//Ss    |
-     N/// NNN ///S///Ss   | Intelligence Search Tool
-     N/// NNN ///S////Ss  |
-     N/// NNN ///S/////S  | Developed by hadnu
-     N/// NNN ///S//////  |
-     nNNn nNNn  sSSSss    | Let's hunt!
-                                      -- Go 1.24
+      ___          ___     
+     /\__\        /\  \    
+    /::|  |      /::\  \    | Welcome to NameSniper
+   /:|:|  |     /:/\ \  \   | Version 1.0
+  /:/|:|  |__  _\:\~\ \  \ 
+ /:/ |:| /\__\/\ \:\ \ \__\ | Intelligence Search Tool
+ \/__|:|/:/  /\:\ \:\ \/__/ | Developed by hadnu
+     |:/:/  /  \:\ \:\__\   
+     |::/  /    \:\/:/  /   | Let's hunt!
+     /:/  /      \::/  /   
+     \/__/        \/__/    
+
+    -- using Go 1.24.0                                      
 `
 	fmt.Println(banner)
 }
+
