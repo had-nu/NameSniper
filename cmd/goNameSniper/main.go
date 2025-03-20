4fa656c9007f5a3a00a958919d1e3af77afd1abe
had-nu <gustavo.leaomelo@gmail.com>
2025-03-20 18:06:53 +0000

    Fix interactive input to handle full names with bufio

--- cmd/goNameSniper/main.go
+++ cmd/goNameSniper/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+    "bufio"
     "fmt"
     "os"
+    "strings"
     "github.com/had-nu/NameSniper/internal/config"
     "github.com/had-nu/NameSniper/internal/counter"
     "github.com/had-nu/NameSniper/internal/search"
@@ -19,16 +21,27 @@ func main() {
         os.Exit(1)
     }
 
-    // Check command-line arguments
-    if len(os.Args) < 2 {
-        fmt.Println("Usage: namesniper [first_name] [surname]")
+    // Prompt for user input
+    fmt.Print(">>> Enter name to search <FirstName Surname>: ")
+    reader := bufio.NewReader(os.Stdin)
+    input, err := reader.ReadString('\n')
+    if err != nil {
+        fmt.Println("Error reading input:", err)
         os.Exit(1)
     }
 
-    firstName := os.Args[1]
+    // Parse input into firstName and surname
+    input = strings.TrimSpace(input)
+    if input == "" {
+        fmt.Println("Error: No name provided. Please enter a name to search.")
+        os.Exit(1)
+    }
+
+    parts := strings.Split(input, " ")
+    firstName := parts[0]
     surname := ""
-    if len(os.Args) > 2 {
-        surname = os.Args[2]
+    if len(parts) > 1 {
+        surname = parts[1]
     }
 
     query := search.BuildQuery(firstName, surname)
